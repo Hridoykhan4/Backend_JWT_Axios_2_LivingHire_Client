b61a6bcafa291064c1bd2c314f6e02d37fdd37bf
Hridoykhan4 <hridoykhan148385@gmail.com>
2025-07-30 00:26:36 +0600

    server and client setUp added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA3JDh5jUTx-AY6sPSz4tlmDXLHPtkj4ew",
+  authDomain: "living-hire.firebaseapp.com",
+  projectId: "living-hire",
+  storageBucket: "living-hire.firebasestorage.app",
+  messagingSenderId: "343784254659",
+  appId: "1:343784254659:web:1f4d163974508059670676"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
